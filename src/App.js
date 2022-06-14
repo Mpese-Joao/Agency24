b4fba668bb6aa7d5209b5dd06bc3d7023d80d8e7
ANIASE <mpesejoao1992@gmail.com>
2022-06-14 23:05:59 +0100

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ScrollToTop from './components/ScrollToTop'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Recommend from './components/Recommend'
+import Testimonial from './components/Testimonial'
+import Footer from './components/Footer'
 
-function App() {
+
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ScrollToTop />
+      <Navbar />
+      <Hero />
+      <Services />
+      <Recommend />
+      <Testimonial />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
